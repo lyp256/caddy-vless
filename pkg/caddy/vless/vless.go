ecee42c3c7b85409747671b5e5326eacce229f50
lyp256 <lyp256@qq.com>
2026-04-13 14:57:47 +0800

    rename go module

--- pkg/caddy/vless/vless.go
+++ pkg/caddy/vless/vless.go
@@ -8,7 +8,7 @@ import (
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	"github.com/google/uuid"
-	"github.com/lyp256/proxy/pkg/vless"
+	"github.com/lyp256/caddy-vless/pkg/vless"
 	"net/http"
 )
 
